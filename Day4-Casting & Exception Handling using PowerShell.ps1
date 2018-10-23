d79a7ab5ebeb35432e3726c4c58a07415aad6c57
yash-hiteshgupta <hitesh.gupta@yash.com>
2018-10-23 15:35:28 +0530

    Exception handing using PS

---
+++ Day4-Casting & Exception Handling using PowerShell.ps1
@@ -0,0 +1,45 @@
+﻿enum Operator {
+    EQUAL = 1
+    GREATER_THAN = 2
+    LESS_THAN = 3
+}
+class Casting {
+
+    IsValid([string]$inputString, [string]$checkString, [int]$operation) {         
+        try{
+                $result = $this.Comparision($inputString, $checkString, $operation)
+
+                Write-Host "Result : $result"
+        }
+        catch
+        {
+            
+        }
+    }
+
+    [bool] Comparision([double]$obj1, [double]$obj2, [Operator]$operation) {
+        Write-Host "Value 1 : $obj1"
+        Write-Host "Value 2 : $obj2"
+        Write-Host "Operation : $operation"
+        switch ($operation) {
+            EQUAL {
+                    return $obj1.Equals($obj2)
+                }
+            GREATER_THAN {
+                    return $obj1.CompareTo($obj2) -gt 0
+                }
+            LESS_THAN {                    
+                    return $obj1.CompareTo($obj2) -lt 0
+                }
+            default {
+                    return False                   
+                }                
+        }
+        return False
+    }
+}
+
+clear
+$a = [Casting]::new();
+$a.IsValid(10,11,3)
+
