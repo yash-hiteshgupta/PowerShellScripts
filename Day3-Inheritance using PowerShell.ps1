374f110c5624ee950af5e60156fd1ecf19baf9b1
yash-hiteshgupta <hitesh.gupta@yash.com>
2018-10-23 11:30:46 +0530

    Inheritance with PowerShell

---
+++ Day3-Inheritance using PowerShell.ps1
@@ -0,0 +1,37 @@
+﻿<#
+Date    :    23-10-2018
+Created By:  Hitesh Gupta
+Description: Sample code for demonstrate Inheritance using PowerShell
+#>
+        
+enum MakeOfCar {
+    Chevy = 1
+    Ford = 2
+    Olds = 3
+    Toyota = 4
+    BMW = 5
+}
+
+enum ColorOfCar {
+    Red = 1
+    Blue = 2
+    Green = 3
+}  
+class Vehicle {
+    [string]$Year
+    [string]$Model
+          
+    }
+
+class Car : Vehicle {
+    [string]$Vin
+    [MakeOfCar]$Make
+    [ColorOfCar]$Color
+}
+
+
+clear
+$a = [Car]::new();
+$a.Color = 1 ; $a.Make = 2 ; $a.Model = "mustang" ; $a.Vin = 12345 ; $a.Year = "01/10/2018";
+$a
+#$a | Get-Member -MemberType Property
---
+++ Exploration Scripts/simple inheritance.ps1
@@ -0,0 +1,78 @@
+﻿enum DeviceType {
+    Undefined = 0
+    Compute = 1
+    Storage = 2
+    Networking = 4
+    Communications = 8
+    Power = 16
+    Rack = 32
+}
+
+class Asset {
+    [string]$Brand
+    [string]$Model
+}
+
+class Device : Asset {
+    hidden [DeviceType]$devtype = [DeviceType]::Undefined
+    [string]$Status
+
+    [DeviceType] GetDeviceType(){
+        return $this.devtype
+    }
+}
+
+class ComputeServer : Device {
+    hidden [DeviceType]$devtype = [DeviceType]::Compute
+    [string]$ProcessorIdentifier
+    [string]$Hostname
+}
+
+class Rack : Device {
+    hidden [DeviceType]$devtype = [DeviceType]::Rack
+    hidden [int]$Slots = 8
+
+    [string]$Datacenter
+    [string]$Location
+    [Device[]]$Devices = [Device[]]::new($this.Slots)
+
+    Rack (){
+        ## Just create the default rack with 8 slots
+    }
+
+    Rack ([int]$s){
+        ## Add argument validation logic here
+        $this.Devices = [Device[]]::new($s)
+    }
+
+    [void] AddDevice([Device]$dev, [int]$slot){
+        ## Add argument validation logic here
+        $this.Devices[$slot] = $dev
+    }
+
+    [void] RemoveDevice([int]$slot){
+        ## Add argument validation logic here
+        $this.Devices[$slot] = $null
+    }
+}
+
+$FirstRack = [Rack]::new(16)
+$FirstRack.Status = "Operational"
+$FirstRack.Datacenter = "PNW"
+$FirstRack.Location = "F03R02.J10"
+
+(0..5).ForEach({
+    $ComputeServer = [ComputeServer]::new()
+    $ComputeServer.Brand = "Fabrikam, Inc."       ## Inherited from Asset
+    $ComputeServer.Model = "Fbk5040"              ## Inherited from Asset
+    $ComputeServer.Status = "Installed"           ## Inherited from Device
+    $ComputeServer.ProcessorIdentifier = "x64"    ## ComputeServer
+    $ComputeServer.Hostname = ("r1s" + $_.ToString("000")) ## ComputeServer
+    $FirstRack.AddDevice($ComputeServer, $_)
+  })
+
+clear
+
+$FirstRack
+
+$FirstRack.Devices
